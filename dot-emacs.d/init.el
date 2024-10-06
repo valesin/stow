8186d37c7b87ecf38da45c554f2bc639eeb89827
hp <hp@hp.it>
2024-10-06 23:39:43 +0200

    vko on 06/10/2024

--- dot-emacs.d/init.el
+++ dot-emacs.d/init.el
@@ -66,10 +66,10 @@
 (fset 'yes-or-no-p 'y-or-n-p)
 
 ;; i3 integration
-;; (require 'i3)
-;; (require 'i3-integration)
-;; (i3-one-window-per-frame-mode-on)
-;; (i3-advise-visible-frame-list-on)
+(require 'i3)
+(require 'i3-integration)
+(i3-one-window-per-frame-mode-on)
+(i3-advise-visible-frame-list-on)
 
 (require 'company)
 (setq company-idle-delay 0)
