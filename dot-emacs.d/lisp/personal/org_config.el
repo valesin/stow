19d778dba14b694d01d8c326bf51f4870d35bb5d
oste <oste@oste.it>
2024-11-14 16:54:16 +0100

    vjo on 14/11/2024

--- dot-emacs.d/lisp/personal/org_config.el
+++ dot-emacs.d/lisp/personal/org_config.el
@@ -115,7 +115,7 @@
 	 "Uni"
 	 :publishing-function org-html-publish-to-html
          :base-directory "/home/vjo/Documents/Notes/Uni/"
-         :publishing-directory "/home/vjo/Documents/Notes/Uni/docs"
+         :publishing-directory "/home/vjo/Documents/docs"
 	 :recursive t
 	 
 	 :with-creator t
