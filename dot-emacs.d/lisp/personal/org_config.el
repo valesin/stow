372090fc66240e2a81121c52fb5e8cad19812cd1
oste <oste@oste.it>
2024-11-15 13:12:09 +0100

    vjo on 15/11/2024

--- dot-emacs.d/lisp/personal/org_config.el
+++ dot-emacs.d/lisp/personal/org_config.el
@@ -129,36 +129,36 @@
 	)
       )
 
-(defun org-html--reference (datum info &optional named-only)
-  "Return an appropriate reference for DATUM.
-DATUM is an element or a `target' type object.  INFO is the
-current export state, as a plist.
-When NAMED-ONLY is non-nil and DATUM has no NAME keyword, return
-nil.  This doesn't apply to headlines, inline tasks, radio
-targets and targets."
-  (let* ((type (org-element-type datum))
-	 (user-label
-	  (org-element-property
-	   (pcase type
-	     ((or `headline `inlinetask) :CUSTOM_ID)
-	     ((or `radio-target `target) :value)
-	     (_ :name))
-	   datum))
-         (user-label (or user-label
-                         (when-let ((path (org-element-property :ID datum)))
-                           (concat "ID-" path)))))
-    (cond
-     ((and user-label
-	   (or (plist-get info :html-prefer-user-labels)
-	       ;; Used CUSTOM_ID property unconditionally.
-	       (memq type '(headline inlinetask))))
-      user-label)
-     ((and named-only
-	   (not (memq type '(headline inlinetask radio-target target)))
-	   (not user-label))
-      nil)
-     (t
-      (org-export-get-reference datum info)))))
+;; (defun org-html--reference (datum info &optional named-only)
+;;   "Return an appropriate reference for DATUM.
+;; DATUM is an element or a `target' type object.  INFO is the
+;; current export state, as a plist.
+;; When NAMED-ONLY is non-nil and DATUM has no NAME keyword, return
+;; nil.  This doesn't apply to headlines, inline tasks, radio
+;; targets and targets."
+;;   (let* ((type (org-element-type datum))
+;; 	 (user-label
+;; 	  (org-element-property
+;; 	   (pcase type
+;; 	     ((or `headline `inlinetask) :CUSTOM_ID)
+;; 	     ((or `radio-target `target) :value)
+;; 	     (_ :name))
+;; 	   datum))
+;;          (user-label (or user-label
+;;                          (when-let ((path (org-element-property :ID datum)))
+;;                            (concat "ID-" path)))))
+;;     (cond
+;;      ((and user-label
+;; 	   (or (plist-get info :html-prefer-user-labels)
+;; 	       ;; Used CUSTOM_ID property unconditionally.
+;; 	       (memq type '(headline inlinetask))))
+;;       user-label)
+;;      ((and named-only
+;; 	   (not (memq type '(headline inlinetask radio-target target)))
+;; 	   (not user-label))
+;;       nil)
+;;      (t
+;;       (org-export-get-reference datum info)))))
 
 
 
