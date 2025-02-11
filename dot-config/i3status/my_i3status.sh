4f5530dd006281d12be15948e57fe2f0419f7413
oste <oste@oste.it>
2025-02-11 18:06:23 +0100

    vjo on 11/02/2025

--- dot-config/i3status/my_i3status.sh
+++ dot-config/i3status/my_i3status.sh
@@ -5,13 +5,13 @@
 # Start background task to send SIGUSR1 every 10 seconds
 (
   while true; do
-    sleep 10
+    sleep 15
     killall -SIGUSR1 i3status
   done
 ) &
 
 # Read the output from i3status line by line
 i3status | while read line; do
-    custom_output=$(~/Documents/Scripts/pomo/pomo -S)
+    custom_output=$(~/Documents/Scripts/pomo/pomo --status)
     echo "$custom_output | $line" || exit 1
 done
